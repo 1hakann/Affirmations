ffd39bf9ffd685968a15968c0ca72a328a72b047
hakan çakmak <hakan.cakmak@live.com>
2021-04-19 00:31:45 +0300

    recyclerview eklendi. teksatır layout düzenlendi. adapter geliştirildi.

--- app/src/main/java/com/badlogic/androidgames/affirmations/MainActivity.kt
+++ app/src/main/java/com/badlogic/androidgames/affirmations/MainActivity.kt
@@ -2,16 +2,17 @@ package com.badlogic.androidgames.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.badlogic.androidgames.affirmations.data.Datasource
-import javax.sql.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myText:TextView = findViewById(R.id.tvText)
-        myText.text = Datasource().loadAffirmations().size.toString()
+        val myDataset = Datasource().loadAffirmations()
+        val myRecycler = findViewById<RecyclerView>(R.id.rvAffirmation)
+        myRecycler.adapter = ItemAdapter(this, myDataset)
+        myRecycler.setHasFixedSize(true)
     }
 }
